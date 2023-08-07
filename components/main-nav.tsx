7c4ead77be9a24dbe51c98137a6479e7f6656b7f
Ndyabagye Henry <ndyabagyehenrytusi@gmail.com>
2023-08-07 16:49:46 +0300

    use origin

--- components/main-nav.tsx
+++ components/main-nav.tsx
@@ -10,9 +10,14 @@ export function MainNav({className, ...props}: React.HTMLAttributes<HTMLElement>
 
     const routes = [
         {
+            href: `/${params.storeId}`,
+            label: "Overview",
+            active: pathname === `/${params.storeId}`,
+        },
+        {
             href: `/${params.storeId}/settings`,
             label: " Settings",
-            active: pathname === `${params.storeId}/settings`,
+            active: pathname === `/${params.storeId}/settings`,
         }];
     return (
         <nav className={cn("flex items-center space-x-4 lg:space-x-6", className)}>
